ad3c6fa0448203c25d1190749b56ec3bcd8e3f5b
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-11 20:20:22 +0530

    updates

---
+++ gulp.js
@@ -0,0 +1,17 @@
+/*
+*
+*Gulp Automation Configuration
+*
+*/
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+gulp.task('default', ['browser-sync']);
