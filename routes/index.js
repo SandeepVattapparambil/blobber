61b7ed8827b1241ee4de167f9d30ba55c0e729e7
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-12 11:41:55 +0530

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+/*Mongoose driver for MongoDB*/
+var mongoose = require('mongoose');
+//Check MongoDB service is running or not
+var connect_db = mongoose.connect('mongodb://localhost/blobber');
+if(connect_db){
+  console.log("\nDatabase connection available!");
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
