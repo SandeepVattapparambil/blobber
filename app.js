4e21eeae4bf53c01232659e5a555a79ca09f7b44
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-12 13:24:13 +0530

    updates

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/login', login);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/login.js
+++ routes/login.js
@@ -6,30 +6,15 @@ var mongoose = require('mongoose');
 var connect_db = mongoose.connect('mongodb://localhost/blobber');
 if (connect_db) {
     console.log("\nDatabase connection available!");
+} else if (error) {
+    console.log(error);
 }
-else if(error){
-  console.log(error);
-}
-// Mongoose Schema definition
-var Schema = mongoose.Schema;
-var UserSchema = new Schema({
-    first_name: String,
-    lastname: String,
-    user_name: String,
-    password: String
-});
-// Mongoose Model definition
-var User = mongoose.model('users', UserSchema);
 
 /* Login page. */
 router.post('/login', function(req, res, next) {
-  if(req.body.username && req.body.password){
-    User.find({username:req.body.username, password:req.body.password}, function(err){
-      res.render('index', {
-          message: 'User Not Found'
-      });
-    });
-  }
+    if (req.body.username && req.body.password) {
+
+    }
 });
 
 module.exports = router;
