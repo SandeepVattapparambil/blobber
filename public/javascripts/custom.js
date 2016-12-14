b9524d7d6b1cb1adb509c11e087464046eec8615
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-14 21:44:44 +0530

    updates

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -26,9 +26,15 @@ $(document).ready(function() {
         belowOrigin: true, // Displays dropdown below the button
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
+    //Material Select init
+    $('select').material_select();
     //Profile form change
     $('#profile').change(function() {
         $('#profile_cancel').toggleClass('disabled');
         $('#profile_update').toggleClass('disabled');
+    });
+    //Add new user
+    $('#add_new_user').click(function(){
+
     });
 });
